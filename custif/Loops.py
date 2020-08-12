af36f610a535bd718f6b18e976693839479cff0a
justin910206 <justin.cory.lamb@gmail.com>
2020-08-12 12:07:05 -0700

    pratice for loops

---
+++ custif/Loops.py
@@ -0,0 +1,11 @@
+# foo = open("ourflie.txt","w")
+#
+# fruitbowl = (apple, pear, snakefruit, orange, bannana)
+#
+#
+# for fruit in fruitbowl:
+#     print(fruit, file=foo)
+stars = ["1", "1 2", "1 2 3", "1 2 3 4", "* * * * *", "* * * *", "* * *", "* *", "*"]
+for each in stars:
+    print(f"{each} \n")
+
