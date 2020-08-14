293c4ad6906a802e10378d4522f6f3fc60e37d45
justin910206 <justin.cory.lamb@gmail.com>
2020-08-14 10:13:09 -0700

    labs from class

---
+++ custif/flowChart.py
@@ -0,0 +1,50 @@
+print("Welcome to my FlowChart")
+
+name = input("Please enter your name")
+
+print("Hello", name, "I hope you enjoy your time with me")
+
+statement1 = input("This chart will assist you on what language you should you learn in 2020")
+statement2 = input("We will assist you by giving you 2 options to help guide you")
+statement3 = input("Here is your first choice")
+
+answer1 = input("Useful or Cool?")
+
+answer1 = 'Cool'
+
+if answer1 == 'Cool':
+    print('You have decided to be Cool')
+elif answer1 == 'Useful':
+    print('You have decided to be Useful')
+else:
+    print('Please pick again')
+
+print("Now", name, "time to choose again")
+
+answer2 = input("Easy or Hard?")
+
+answer2 = 'Easy'
+
+if answer2 == 'Easy':
+    print('YOUR LANGUAGE IS SPANISH!!')
+elif answer2 == 'Hard':
+    print('You have decided to take the Hard route')
+else:
+    print('Please pick again')
+
+
+
+print("Now", name, "time to choose again")
+
+answer3 = input("Phonetcs or Grammar")
+
+answer3 = 'Phonetcs'
+
+if answer3 == 'Phonetcs':
+    print('YOUR LANGUAGE IS MANDARIN!!')
+elif answer3 == 'Grammar':
+    print('YOUR LANGUAGE IS JAPANESE')
+else:
+    print('Please pick again')
+
+
---
+++ custif/slicingChallenge.py
@@ -0,0 +1,21 @@
+# easy= ["science", "turbo", ["goggles", "eyes"], "nothing"]
+#
+# print(easy[3])
+# print(easy[2][0])
+# print(easy[2][1])
+
+#!/usr/bin/env python3
+import urllib.request
+import re
+
+print("Where should we search?")
+url = input()
+print("Great! So we'll try to open this url " + str(url) + "to search for the phrase:")
+searchFor = input()
+searchMe = urllib.request.urlopen(url).read().decode("utf-8")
+
+if re.search(searchFor, searchMe):
+    print("Found a match!")
+else:
+    print("No match!")
+
